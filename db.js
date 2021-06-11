5ad8b7acd91b660766f59532307bd459cd2867ec
charm <404373224@qq.com>
2021-06-11 18:26:18 +0800

    fix db

--- db.js
+++ db.js
@@ -146,7 +146,8 @@ module.exports.TokenTransfer = mongoose.model('TokenTransfer');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/explorerDB', {
-  useMongoClient: true
+  useMongoClient: true,
+  useNewUrlParser: true,
   // poolSize: 5,
   // rs_name: 'myReplicaSetName',
   // user: 'explorer',
